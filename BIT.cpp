5404e7a3052bdbca0d2f46e05079e9adfeeeee21
yeedrag <andywangntueees2005722@gmail.com>
2023-12-12 10:47:56 -0600

    changed indent

--- BIT.cpp
+++ BIT.cpp
@@ -8,74 +8,74 @@
 #define int ll
 using namespace std;
 /*
-    Structure for Binary-Indexed-Tree (BIT).
-    Usage:
-        Implementation is 1-based index, range sum single point update only.
-        Can be used for any operation that satisfys the associative property.
-        Can also calculate suffix by swapping for loops, or let idx = BIT::sz - idx;
-        Initialize with BIT (name)(int sz) to initialize a BIT with zeros with length n.
-        .sum(int r) : obtain prefix sum till index r (inclusive).
-        .sum(int l, int r) : obtain sum from index l to index r (inclusive).
-        .add(int idx, int val) : add val in index idx.
-    Time complexity:
-        initialize: O(N)
-        sum: O(NlogN)
-        add: O(NlogN)
+	Structure for Binary-Indexed-Tree (BIT).
+	Usage:
+		Implementation is 1-based index, range sum single point update only.
+		Can be used for any operation that satisfys the associative property.
+		Can also calculate suffix by swapping for loops, or let idx = BIT::sz - idx;
+		Initialize with BIT (name)(int sz) to initialize a BIT with zeros with length n.
+		.sum(int r) : obtain prefix sum till index r (inclusive).
+		.sum(int l, int r) : obtain sum from index l to index r (inclusive).
+		.add(int idx, int val) : add val in index idx.
+	Time complexity:
+		initialize: O(N)
+		sum: O(NlogN)
+		add: O(NlogN)
 */
  
 struct BIT {
-    vector<int> bit;
-    int sz; // size of BIT
-    int lowbit(int n) {return (n & -n);}
-    BIT(int sz) {
-        this -> sz = sz + 1; // 1 is added because 0 ~ n
-        bit.assign(sz + 1, 0);
-    }
-    int sum(int idx) {
-        int ret = 0;
-        for(; idx > 0; idx -= lowbit(idx)) ret += bit[idx];
-        return ret;
-    }
-    int sum(int l, int r) {return sum(r) - sum(l - 1);}
-    void add(int idx, int val) {
-        for(; idx <= sz - 1; idx += lowbit(idx)) {
-            bit[idx] += val;
-        }
-    }
+	vector<int> bit;
+	int sz; // size of BIT
+	int lowbit(int n) {return (n & -n);}
+	BIT(int sz) {
+		this -> sz = sz + 1; // 1 is added because 0 ~ n
+		bit.assign(sz + 1, 0);
+	}
+	int sum(int idx) {
+		int ret = 0;
+		for(; idx > 0; idx -= lowbit(idx)) ret += bit[idx];
+		return ret;
+	}
+	int sum(int l, int r) {return sum(r) - sum(l - 1);}
+	void add(int idx, int val) {
+		for(; idx <= sz - 1; idx += lowbit(idx)) {
+			bit[idx] += val;
+		}
+	}
 };
  
 /*
-    Below is implementation for codeforces 61E. Enemy is weak.
-    Find the number of triplets such that i < j < k and a_i > a_j > a_k.
+	Below is implementation for codeforces 61E. Enemy is weak.
+	Find the number of triplets such that i < j < k and a_i > a_j > a_k.
 */
 signed main() {
-    guracute;
-    int n;
-    cin >> n;
-    vector<int> vec(n);
-    BIT prefix(n), suffix(n);
-    for(int i = 0; i < n; i++) cin >> vec[i];
-    vector<int> vec2 = vec;
-    sort(all(vec2));
-    uni(vec2);
-    vector<int> discrete_vec(n + 1);
-    for(int i = 1; i <= n; i++) {
-        discrete_vec[i] = (lower_bound(all(vec2), vec[i - 1]) - vec2.begin()) + 1; 
-    }
-    vector<int> suffix_vec(n, 0); // save i < j && a_i > a_j 
-    vector<int> prefix_vec(n, 0); // save j < k && a_j > a_k
-    for(int i = 1; i <= n; i++) {
-        suffix_vec[i] = suffix.sum(n - discrete_vec[i] + 1);
-        suffix.add(n - discrete_vec[i] + 1, 1);
-    }
-    for(int i = n; i >= 1; i--) {
-        prefix_vec[i] = prefix.sum(discrete_vec[i]);
-        prefix.add(discrete_vec[i], 1);
-    }
-    int ans = 0;
-    for(int i = 1; i <= n; i++) {
-        ans += prefix_vec[i] * suffix_vec[i];
-    }
-    cout << ans << endl;
+	guracute;
+	int n;
+	cin >> n;
+	vector<int> vec(n);
+	BIT prefix(n), suffix(n);
+	for(int i = 0; i < n; i++) cin >> vec[i];
+	vector<int> vec2 = vec;
+	sort(all(vec2));
+	uni(vec2);
+	vector<int> discrete_vec(n + 1);
+	for(int i = 1; i <= n; i++) {
+		discrete_vec[i] = (lower_bound(all(vec2), vec[i - 1]) - vec2.begin()) + 1; 
+	}
+	vector<int> suffix_vec(n + 1, 0); // save i < j && a_i > a_j 
+	vector<int> prefix_vec(n + 1, 0); // save j < k && a_j > a_k
+	for(int i = 1; i <= n; i++) {
+		suffix_vec[i] = suffix.sum(n - discrete_vec[i] + 1);
+		suffix.add(n - discrete_vec[i] + 1, 1);
+	}
+	for(int i = n; i >= 1; i--) {
+		prefix_vec[i] = prefix.sum(discrete_vec[i]);
+		prefix.add(discrete_vec[i], 1);
+	}
+	int ans = 0;
+	for(int i = 1; i <= n; i++) {
+		ans += prefix_vec[i] * suffix_vec[i];
+	}
+	cout << ans << endl;
 	return 0;
 }
--- debug.h
+++ debug.h
@@ -1,150 +1,141 @@
 // template from https://github.com/sanathkumarx/Competitive-Coding/blob/master/CodeForces/practice/algo/debug.h
 #undef _GLIBCXX_DEBUG
- 
+
 #include <bits/stdc++.h>
- 
+
 using namespace std;
- 
+
 template <typename A, typename B>
 string to_string(pair<A, B> p);
- 
+
 template <typename A, typename B, typename C>
 string to_string(tuple<A, B, C> p);
- 
+
 template <typename A, typename B, typename C, typename D>
 string to_string(tuple<A, B, C, D> p);
- 
+
 string to_string(const string& s) {
-  return '"' + s + '"';
+	return '"' + s + '"';
 }
- 
+
 string to_string(const char* s) {
-  return to_string((string) s);
+	return to_string((string) s);
 }
- 
+
 string to_string(bool b) {
-  return (b ? "true" : "false");
+	return (b ? "true" : "false");
 }
- 
+
 string to_string(vector<bool> v) {
-  bool first = true;
-  string res = "{";
-  for (int i = 0; i < static_cast<int>(v.size()); i++) {
-    if (!first) {
-      res += ", ";
-    }
-    first = false;
-    res += to_string(v[i]);
-  }
-  res += "}";
-  return res;
+	bool first = true;
+	string res = "{";
+	for (int i = 0; i < static_cast<int>(v.size()); i++) {
+	if (!first) {
+		res += ", ";
+	}
+	first = false;
+	res += to_string(v[i]);
+	}
+	res += "}";
+	return res;
 }
 
 string to_string(vector<char> v) {
-  bool first = true;
-  string res = "{";
-  for (int i = 0; i < static_cast<int>(v.size()); i++) {
-      res += v[i];
-      if(i != static_cast<int>(v.size()) - 1) res += ", ";
-  }
-  res += "}";
-  return res;
+	bool first = true;
+	string res = "{";
+	for (int i = 0; i < static_cast<int>(v.size()); i++) {
+		res += v[i];
+		if(i != static_cast<int>(v.size()) - 1) res += ", ";
+	}
+	res += "}";
+	return res;
 }
- 
+
 template <size_t N>
 string to_string(bitset<N> v) {
-  string res = "";
-  for (size_t i = 0; i < N; i++) {
-    res += static_cast<char>('0' + v[i]);
-  }
-  return res;
+	string res = "";
+	for (size_t i = 0; i < N; i++) {
+		res += static_cast<char>('0' + v[i]);
+	}
+	return res;
 }
- 
+
 template <typename A>
 string to_string(A v) {
-  bool first = true;
-  string res = "{";
-  for (const auto &x : v) {
-    if (!first) {
-      res += ", ";
-    }
-    first = false;
-    res += to_string(x);
-  }
-  res += "}";
-  return res;
+	bool first = true;
+	string res = "{";
+	for (const auto &x : v) {
+		if (!first) {
+			res += ", ";
+		}
+		first = false;
+		res += to_string(x);
+	}
+	res += "}";
+	return res;
 }
  
 template <typename A, typename B>
 string to_string(pair<A, B> p) {
-  return "(" + to_string(p.first) + ", " + to_string(p.second) + ")";
+	return "(" + to_string(p.first) + ", " + to_string(p.second) + ")";
 }
- 
+
 template <typename A, typename B, typename C>
 string to_string(tuple<A, B, C> p) {
-  return "(" + to_string(get<0>(p)) + ", " + to_string(get<1>(p)) + ", " + to_string(get<2>(p)) + ")";
+	return "(" + to_string(get<0>(p)) + ", " + to_string(get<1>(p)) + ", " + to_string(get<2>(p)) + ")";
 }
- 
+
 template <typename A, typename B, typename C, typename D>
 string to_string(tuple<A, B, C, D> p) {
-  return "(" + to_string(get<0>(p)) + ", " + to_string(get<1>(p)) + ", " + to_string(get<2>(p)) + ", " + to_string(get<3>(p)) + ")";
+	return "(" + to_string(get<0>(p)) + ", " + to_string(get<1>(p)) + ", " + to_string(get<2>(p)) + ", " + to_string(get<3>(p)) + ")";
 }
 
 template <typename A>
 string to_string(queue<A> q) {
-    vector<A> v;
-    while(q.size()) {
-        v.push_back(q.front());
-        q.pop();
-    }
-    return to_string(v);
+	vector<A> v;
+	while(q.size()) {
+		v.push_back(q.front());
+		q.pop();
+	}
+	return to_string(v);
 }
 
 template <typename A>
 string to_string(stack<A> stk) {
-    vector<A> v;
-    while(stk.size()) {
-        v.push_back(stk.top());
-        stk.pop();
-    }
-    return to_string(v);
+	vector<A> v;
+	while(stk.size()) {
+		v.push_back(stk.top());
+		stk.pop();
+	}
+	return to_string(v);
 }
 
 template <typename A>
 string to_string(deque<A> deq) {
-    vector<A> v;
-    while(deq.size()) {
-        v.push_back(deq.front());
-        deq.pop_front();
-    }
-    return to_string(v);
+	vector<A> v;
+	while(deq.size()) {
+		v.push_back(deq.front());
+		deq.pop_front();
+	}
+	return to_string(v);
 }
 
 template <typename A>
 string to_string(priority_queue<A> pq) {
-    vector<A> v;
-    while(pq.size()) {
-        v.push_back(pq.top());
-        pq.pop();
-    }
-    return to_string(v);
+	vector<A> v;
+	while(pq.size()) {
+		v.push_back(pq.top());
+		pq.pop();
+	}
+	return to_string(v);
 }
 
-template <typename A>
-string to_string(A arr[]) {
-    vector<A> v;
-    for(int i = 0; i < sizeof(arr); i++) {
-        v.pb(arr[i]);
-    }
-    return to_string(v);
-}
- 
 void debug_out() { cerr << endl; }
- 
+
 template <typename Head, typename... Tail>
 void debug_out(Head H, Tail... T) {
-  cerr << " " << to_string(H);
-  debug_out(T...);
+	cerr << " " << to_string(H);
+	debug_out(T...);
 }
 
 #define debug(...) cerr << "[" << #__VA_ARGS__ << "]:", debug_out(__VA_ARGS__)
